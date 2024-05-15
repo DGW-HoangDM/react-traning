5b1a388d06e8f5bccba10b1c0aa288b97a2aa5fd
DGW-HoangDM <hoang-dm@dgw.asia>
2024-05-15 13:57:11 +0700

    define state in class component

--- react-basic/src/views/expample/MyComponent.js
+++ react-basic/src/views/expample/MyComponent.js
@@ -1,11 +1,22 @@
 import React from 'react'
 
 class MyComponent extends React.Component {
+  //define global state 
+  state = {
+    company: 'DGWA'
+  }
+
   render() {
     return (
-      <div>
-        <h1>Hello, {this.props.name}</h1>
-      </div>
+      <>
+        <div>
+          <h1>Hello, {this.props.name}</h1>
+        </div>
+
+        <div>
+          <h1>Hello, {this.state.company}</h1>
+        </div>
+      </>
     )
   }
 }
