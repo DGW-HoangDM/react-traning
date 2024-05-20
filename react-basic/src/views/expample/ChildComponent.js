42afe12ad6b17d3dd7e25ae7740ee8df0e44f92f
DGW-HoangDM <hoang-dm@dgw.asia>
2024-05-20 16:44:00 +0700

    Stateless/StateFull Components

--- react-basic/src/views/expample/ChildComponent.js
+++ react-basic/src/views/expample/ChildComponent.js
@@ -1,19 +1,35 @@
 import React from 'react'
 
-class ChildComponent extends React.Component {
-  render() {
-    console.log('check props:', this.props);
-    //ES6 Destructuring
-    let {name, age} = this.props;
-    
-    return (
-      <>
-        <div>
-          <h1>Hello my child, <i>{name}-{age}</i></h1>
-        </div>
-      </>
-    )
-  }
+//StateFull (class component)
+// class ChildComponent extends React.Component {
+//   render() {
+//     console.log('check props:', this.props);
+//     //ES6 Destructuring
+//     let {name, age} = this.props;
+
+//     return (
+//       <>
+//         <div>
+//           <h1>Hello my child, <i>{name}-{age}</i></h1>
+//         </div>
+//       </>
+//     )
+//   }
+// }
+
+//Stateless (function component)
+function ChildComponent(props) {
+  console.log('check props stateless:', props);
+  //ES6 Destructuring
+  let {name, age} = props;
+  
+  return (
+    <>
+      <div>
+        <h1>Hello my child, <i>{name}-{age}</i></h1>
+      </div>
+    </>
+  )
 }
 
 export default ChildComponent;
