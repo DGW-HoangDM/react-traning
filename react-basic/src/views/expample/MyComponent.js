1fb8d69e4a5be08326f04a01c0fef877f1bb2759
DGW-HoangDM <hoang-dm@dgw.asia>
2024-05-21 14:16:24 +0700

    Conditional Output

--- react-basic/src/views/expample/MyComponent.js
+++ react-basic/src/views/expample/MyComponent.js
@@ -4,18 +4,27 @@ import ChildComponent from './ChildComponent';
 class MyComponent extends React.Component {
   //define global state 
   state = {
-    company: 'DGWA'
+    company: 'DGWA',
+    isChecked: false,
   }
 
   handleClick = (e) =>{
     // console.log('handleClick', e);
     this.setState({
-      company: 'DGWA Viet Nam'
+      company: 'DGWA Viet Nam',
+    })
+  }
+
+  handleShowContent = (e) => {
+    this.setState({
+      isChecked: !this.state.isChecked,
     })
   }
 
   render() {
-    console.log('state', this.state);
+    // console.log('state', this.state);
+    let {isChecked} = this.state;
+
     return (
       <>
         <ChildComponent name="This my soon" age={25}/>
@@ -27,8 +36,23 @@ class MyComponent extends React.Component {
         <div>
           <h1>Hello, {this.state.company}</h1>
         </div>
+        <div className="line"></div>
 
+        {
+          !isChecked ? <button onClick={(e) => this.handleShowContent(e)}>Show content</button>
+          :
+          <>
+            <div>
+              <h1>The content</h1>
+            </div>
+            <button onClick={(e) => this.handleShowContent(e)}>Hide content</button>
+          </>
+        }
+        
+
+        <div className="line"></div>
         <button onClick={(e) => this.handleClick(e)}>Click me</button>
+        
       </>
     )
   }
