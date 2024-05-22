598d1d787b23f659976d9489c7505b5c697fabac
DGW-HoangDM <hoang-dm@dgw.asia>
2024-05-22 10:34:26 +0700

    remove job

--- react-basic/src/views/expample/ParentComponent.js
+++ react-basic/src/views/expample/ParentComponent.js
@@ -4,37 +4,48 @@ import AddComponent from './AddComponent';
 class ParentComponent extends React.Component {
   state = {
     jobList: [
-      {id: 'job1', name: 'Web', salary: 500},
-      {id: 'job2', name: 'Java', salary: 1000},
-      {id: 'job3', name: 'Net', salary: 3000}
-    ]
+      { id: 'job1', name: 'Web', salary: 500 },
+      { id: 'job2', name: 'Java', salary: 1000 },
+      { id: 'job3', name: 'Net', salary: 3000 }
+    ],
   }
 
   handleMessageChange = (job) => {
-    // console.log('job', job)
-    if(job.name !== '' && job.salary !== ''){
-      this.setState({ 
+    if (job.name !== '' && job.salary !== '') {
+      this.setState({
         jobList: [...this.state.jobList, job]
       });
     }
   }
 
+  // Method to handle removing an item
+  removeItem = (id) => {
+    this.setState(prevState => ({
+      jobList: prevState.jobList.filter(item => item.id !== id)
+    }));
+  };
+
   render() {
-    let { jobList } =  this.state;
+    let { jobList } = this.state;
     return (
       <>
         <h1>Parent component</h1>
 
-        <AddComponent 
-          name="This my daughter" 
-          age={5} 
-          onMessageChange = { this.handleMessageChange }
+        <AddComponent
+          name="This my daughter"
+          age={5}
+          onMessageChange={this.handleMessageChange}
         />
         <div>
           <ul>
             {
-              jobList.map((item, index) => {
-                return <li key={item.id}>{item.name}-{item.salary}</li>
+              jobList.map((item) => {
+                return (
+                  <li key={item.id}>
+                    {item.name}-{item.salary}
+                    <span onClick={() => this.removeItem(item.id)}> - X</span>
+                  </li>
+                )
               })
             }
           </ul>
