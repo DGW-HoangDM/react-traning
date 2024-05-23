2acf6bc131ced73d95304f928c26b88db53f3742
DGW-HoangDM <hoang-dm@dgw.asia>
2024-05-23 15:51:44 +0700

    Update todo task

--- react-basic/src/views/todos/ListTodo.js
+++ react-basic/src/views/todos/ListTodo.js
@@ -17,7 +17,8 @@ class ListTodo extends React.Component {
         id: 'task3',
         title: 'An item 3'
       },
-    ]
+    ],
+    editTodo: {}
   }
 
   handleAddTask = (task) => {
@@ -47,27 +48,84 @@ class ListTodo extends React.Component {
     notify();
   }
 
+  handleEditTask = (task) => {
+    let { listTodo, editTodo } = this.state;
+    let isEmptyObject = Object.keys(editTodo).length === 0;
+
+    //check case Update
+    if (!isEmptyObject && task.id === editTodo.id) {
+      //Không edit trực tiếp mà tạo bản copy rồi edit
+      let listTodoCopy = [...listTodo];
+
+      //Find index of specific object using findIndex method.    
+      let objIndex = listTodoCopy.findIndex(item => item.id === editTodo.id);
+      //Update object's name property.
+      listTodoCopy[objIndex].title = editTodo.title;
+
+      this.setState({
+        listTodo: listTodoCopy,
+        editTodo: {}
+      });
+
+      const notify = () => toast.success('Update succeed!');
+      notify();
+      return;
+    }
+
+    //Case Edit
+    this.setState({
+      editTodo: task,
+    });
+  }
+
+  handleEditChange = (e) => {
+    //Không edit trực tiếp mà tạo bản copy rồi edit
+    let editTodoCopy = { ...this.state.editTodo };
+    editTodoCopy.title = e.target.value;
+
+    this.setState({
+      editTodo: editTodoCopy
+    })
+  }
 
   render() {
-    let { listTodo } = this.state;
+    let { listTodo, editTodo } = this.state;
+    let isEmptyObject = Object.keys(editTodo).length === 0;
 
     return (
       <>
         <div className="content">
-          <AddTodo 
-            onHandleAddTask= { this.handleAddTask}
+          <AddTodo
+            onHandleAddTask={this.handleAddTask}
           />
-          
+
           <ul className="list-group">
             {
               listTodo && listTodo.length > 0 &&
               listTodo.map((todo) => {
                 return (
                   <li className="list-group-item d-flex justify-content-between" key={todo.id}>
-                    <span>{todo.title}</span>
+                    {
+                      isEmptyObject ?
+                        <span>{todo.title}</span>
+                        :
+                        <>
+                          {
+                            todo.id === editTodo.id ?
+                              <span>
+                                <input type="text" value={editTodo.title} onChange={(e) => this.handleEditChange(e)} />
+                              </span>
+                              :
+                              <span>{todo.title}</span>
+                          }
+                        </>
+                    }
+
                     <div className="d-flex gap-3">
-                      <button className="btn btn-info mr-3" type="button" >Edit</button>
-                      <button className="btn btn-danger" type="button" onClick={ (e) => this.handleDeleteTask(todo.id)}>Delete</button>
+                      <button className="btn btn-info mr-3" type="button" onClick={(e) => this.handleEditTask(todo)} >
+                        {isEmptyObject === false && todo.id === editTodo.id ? 'Update' : 'Edit'}
+                      </button>
+                      <button className="btn btn-danger" type="button" onClick={(e) => this.handleDeleteTask(todo.id)}>Delete</button>
                     </div>
                   </li>
                 )
